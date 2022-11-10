30a0499a9df50e67ea616f33c8502ec5f4a4b73a
Nick <Nick@192.168.1.2>
2022-11-10 18:10:29 +0200

    End of Project

--- TravelCompany-master/TravelCompany-master/src/main/java/com/travelcompany/eshop/service/PurchaseService.java
+++
@@ -1,8 +0,0 @@
-package com.travelcompany.eshop.service;
-
-import com.travelcompany.eshop.exception.PurchaseException;
-
-public interface PurchaseService {
-
-    void purchase(Integer passengerId, Integer itineraryId, String paymentMethod) throws PurchaseException;
-}
